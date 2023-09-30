5f79a00b598511ce9d4237cf052023f3aa720327
Fernando Andana <fernando_andana_@hotmail.com>
2023-09-30 03:44:09 -0300

    Se agrego la clase test para Util.java

--- src/main/java/org/core/utilidades/Util.java
+++ src/main/java/org/core/utilidades/Util.java
@@ -1,5 +1,45 @@
 package org.core.utilidades;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Util {
 
+    public static String getFechaFormato(Date date, String formato){
+        SimpleDateFormat sdf = new SimpleDateFormat(formato);
+        return sdf.format(date);
+    }
+    public static Date getFechaHoy(){
+        return Calendar.getInstance().getTime();
+    }
+    public static Date getPrimeraHora(Date date){
+        return transformarTiempoPrimeraHora(date);
+    }
+    public static Date getUltimaHora(Date date){
+        return transformarTiempoUltimaHora(date);
+    }
+    private static Date transformarTiempoPrimeraHora(Date date){
+        Date fecha = (Date) date.clone();
+        date.setSeconds(0);
+        date.setMinutes(0);
+        date.setHours(0);
+        return fecha;
+    }
+    private static Date transformarTiempoUltimaHora(Date date){
+        Date fecha = (Date) date.clone();
+        fecha.setSeconds(59);
+        fecha.setMinutes(59);
+        fecha.setHours(23);
+        return fecha;
+    }
+    public static boolean compararEntreFechas(Date fechaAnterior, Date fechaPosterior, Date fecha){
+        return fecha.after(fechaAnterior) && fecha.before(fechaPosterior);
+    }
+    public static boolean esFechaAnterior(Date fechaAnterior, Date fecha){
+        return fechaAnterior.before(fecha);
+    }
+    public static boolean esFechaPosterior(Date fechaPosterior, Date fecha){
+        return fechaPosterior.after(fecha);
+    }
 }
---
+++ src/test/java/org/core/utilidades/UtilTest.java
@@ -0,0 +1,76 @@
+package org.core.utilidades;
+
+import org.junit.jupiter.api.Test;
+import java.util.Calendar;
+import java.util.Date;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+    private final Date fechaHoy = Calendar.getInstance().getTime();
+
+    @Test
+    public void deberiaDevolverFormatoddMMyyyy(){
+        String fechaEsperada = "30/09/2023";
+        Date date = fechaHoy;
+        date.setMonth(Calendar.SEPTEMBER);
+        date.setYear(123); // 1900 + 123 = 2023
+        date.setDate(30);
+        String fechaObtenida = Util.getFechaFormato(date, "dd/MM/yyyy");
+        assertEquals( fechaEsperada, fechaObtenida, "El formato no es el mismo");
+    }
+
+    @Test
+    public void deberiaDevolverLaFechaDeHoy(){
+        Date fechaObtenida = Util.getFechaHoy();
+        assertThat(fechaHoy).isCloseTo(fechaObtenida, 900L);
+    }
+
+    @Test
+    public void deberiaDevolverLaPrimeraHoraDeHoy(){
+        Date fechaEsperada = fechaHoy;
+        fechaEsperada.setSeconds(0);
+        fechaEsperada.setMinutes(0);
+        fechaEsperada.setHours(0);
+        Date fechaObtenida = Util.getPrimeraHora(fechaHoy);
+        assertThat(fechaEsperada).isCloseTo(fechaObtenida, 100L);
+    }
+
+    @Test
+    public void deberiaDevolverLaUltimaHoraDeHoy(){
+        Date fechaEsperada = Calendar.getInstance().getTime();
+        fechaEsperada.setSeconds(59);
+        fechaEsperada.setMinutes(59);
+        fechaEsperada.setHours(23);
+        Date fechaObtenida = Util.getUltimaHora(fechaHoy);
+        assertThat(fechaEsperada).isCloseTo(fechaObtenida, 100L);
+    }
+
+    @Test
+    public void deberiaCompararEntreFechas(){
+        Date fechaAnterior = new Date(123, Calendar.SEPTEMBER, 29);  // 29/09/2023
+        Date fechaPosterior = new Date(123, Calendar.OCTOBER, 1);    // 01/10/2023
+        Date fechaAComparar = new Date(123, Calendar.SEPTEMBER, 30); // 30/09/2023
+
+        assertTrue(Util.compararEntreFechas(fechaAnterior, fechaPosterior, fechaAComparar), "La fecha "
+                + Util.getFechaFormato(fechaAComparar, "dd/MM/yyyy")
+                + " no esta entre "
+                + Util.getFechaFormato(fechaAnterior, "dd/MM/yyyy")
+                + " y "
+                + Util.getFechaFormato(fechaPosterior, "dd/MM/yyyy"));
+    }
+
+    @Test
+    public void deberiaComprobarSiFechaEsAnteriorAOTra(){
+        Date fechaAnterior = new Date(123, Calendar.SEPTEMBER, 29);  // 29/09/2023
+        Date fechaAComparar = new Date(123, Calendar.SEPTEMBER, 30); // 30/09/2023
+        assertTrue(Util.esFechaAnterior(fechaAnterior, fechaAComparar));
+    }
+
+    @Test
+    public void deberiaComprobarSiFechaEsPosteriorAOtra(){
+        Date fechaPosterior = new Date(123, Calendar.OCTOBER, 1);    // 01/10/2023
+        Date fechaAComparar = new Date(123, Calendar.SEPTEMBER, 30); // 30/09/2023
+        assertTrue(Util.esFechaPosterior(fechaPosterior, fechaAComparar));
+    }
+}
